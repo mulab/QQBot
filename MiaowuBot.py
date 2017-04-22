75a023e87b479948d1731ecb8d247883fc59c5fe
Hubertzhang <hubert.zyk@gmail.com>
2017-04-22 15:22:28 +0800

    add regex for miaowu

--- MiaowuBot.py
+++ MiaowuBot.py
@@ -1,6 +1,8 @@
 import os
 import random
 import json
+import re
+
 from plugin import Plugin
 import redis
 
@@ -16,6 +18,7 @@ class MiaowuBot(Plugin):
     def __init__(self):
         self.reply_data = dict()
         self.trigger_data = dict()
+        self.regex_data = dict()
         self.use_redis = False
         self.database = None
 
@@ -29,7 +32,9 @@ class MiaowuBot(Plugin):
             all_groups = list(map(bytes.decode, self.database.smembers("valid_group")))
             for group in all_groups:
                 triggers = set(map(bytes.decode, self.database.smembers('miaowu:{}'.format(group))))
+                regexs = set(map(bytes.decode, self.database.smembers('miaowu.regex:{}'.format(group))))
                 self.trigger_data[group] = triggers
+                self.regex_data[group] = regexs
             return
         else:
             self.filepath = os.path.join(data_path, "reply_data.json")
@@ -42,7 +47,7 @@ class MiaowuBot(Plugin):
         return
 
     def supported_commands(self):
-        return ['!add', '!del', '!list', '!trigger']
+        return ['!add', '!del', '!list', '!trigger', '!addreg', '!delreg']
 
     def command_received(self, command, content, messageInfo):
         gnumber = messageInfo['group_uid']
@@ -67,6 +72,29 @@ class MiaowuBot(Plugin):
             r = del_message[1].strip()
 
             return self.del_trigger(gnumber, trigger, r)
+        elif command == '!addreg':
+            add_message = content.strip().split('#', 1)
+            if len(add_message) < 2:
+                print("not enough", add_message)
+                return ''
+            trigger = add_message[0].strip()
+            try:
+                re.compile(trigger)
+                if len(trigger) < 2:
+                    return 'Trigger must > 2'
+                r = add_message[1].strip()
+                print("add", trigger, r)
+                return self.add_regex_trigger(gnumber, trigger, r)
+            except re.error:
+                return "Can't compile that expression"
+        elif command == '!delreg':
+            del_message = content.strip().split('#', 1)
+            if len(del_message) < 2:
+                return ''
+            trigger = del_message[0].strip()
+            r = del_message[1].strip()
+
+            return self.del_regex_trigger(gnumber, trigger, r)
         elif command == '!list':
             trigger = content.strip()
             messages = self.get_trigger_message(gnumber, trigger)
@@ -77,6 +105,9 @@ class MiaowuBot(Plugin):
         elif command == '!trigger':
             message = content.strip()
             replys = []
+            for key in self.get_regex_trigger(gnumber):
+                if re.search(key, message) is not None:
+                    replys.append(key)
             for key in self.get_trigger(gnumber):
                 if key in message:
                     replys.append(key)
@@ -88,9 +119,16 @@ class MiaowuBot(Plugin):
         gnumber = message['group_uid']
         message_content = message['content']
         replys = []
+        for key in self.get_regex_trigger(gnumber):
+            reg_result = re.search(key, message_content)
+            if reg_result:
+                replace_string = self.get_regex_random_reply(gnumber, key)
+                reg_string = reg_result.group()
+                reply_string = re.sub(key, replace_string, reg_string)
+                replys.append(reply_string)
         for key in self.get_trigger(gnumber):
             if key in message_content:
-                replys.append(self.get_ramdom_reply(gnumber, key))
+                replys.append(self.get_regex_random_reply(gnumber, key))
         if len(replys) > 0:
             return random.choice(replys)
         return ''
@@ -115,6 +153,15 @@ class MiaowuBot(Plugin):
                 self.reply_data[group_id] = dict()
             return self.reply_data[group_id]
 
+    def get_regex_trigger(self, group_id):
+        if self.use_redis:
+            ret = self.regex_data.get(str(group_id))
+            if ret is None:
+                ret = set(map(bytes.decode, self.database.smembers('miaowu.regex:{}'.format(group_id))))
+                self.regex_data[str(group_id)] = ret
+            return ret
+        return set()
+
     def get_trigger_message(self, group_id, trigger):
         if self.use_redis:
             return list(map(bytes.decode, self.database.smembers("miaowu:{}:{}".format(group_id, trigger))))
@@ -123,9 +170,23 @@ class MiaowuBot(Plugin):
                 self.reply_data[group_id] = dict()
             return self.reply_data[group_id][trigger]
 
-    def get_ramdom_reply(self, group_id, trigger):
+    def get_regex_trigger_message(self, group_id, trigger):
+        if self.use_redis:
+            return list(map(bytes.decode, self.database.smembers("miaowu.regex:{}:{}".format(group_id, trigger))))
+        return []
+
+    def get_random_reply(self, group_id, trigger):
         if self.use_redis:
             return self.database.srandmember("miaowu:{}:{}".format(group_id, trigger)).decode()
+        else:
+            all_reply = self.reply_data[group_id][trigger]
+            if len(all_reply) > 0:
+                reply = random.choice(all_reply)
+                return reply
+
+    def get_regex_random_reply(self, group_id, trigger):
+        if self.use_redis:
+            return self.database.srandmember("miaowu.regex:{}:{}".format(group_id, trigger)).decode()
         else:
             all_reply = self.reply_data[group_id][trigger]
             if len(all_reply) > 0:
@@ -139,6 +200,22 @@ class MiaowuBot(Plugin):
             self.trigger_data[str(group_id)].add(trigger)
             self.database.sadd("miaowu:{}".format(group_id), trigger)
             if self.database.sadd("miaowu:{}:{}".format(group_id, trigger), message) == 1:
+                return "Added"
+            else:
+                return "Reply already exists"
+        else:
+            if self.reply_data[group_id].get(trigger, None) is None:
+                self.reply_data[group_id][trigger] = []
+            self.reply_data[group_id][trigger].append(message)
+            return 'Added'
+
+    def add_regex_trigger(self, group_id, trigger, message):
+        if self.use_redis:
+            if self.regex_data.get(str(group_id)) is None:
+                self.regex_data[str(group_id)] = set()
+            self.regex_data[str(group_id)].add(trigger)
+            self.database.sadd("miaowu.regex:{}".format(group_id), trigger)
+            if self.database.sadd("miaowu.regex:{}:{}".format(group_id, trigger), message) == 1:
                 return "Added"
             else:
                 return "Reply already exists"
@@ -173,3 +250,21 @@ class MiaowuBot(Plugin):
                 return "Removed"
             else:
                 return 'Reply not exist'
+
+    def del_regex_trigger(self, group_id, trigger, message):
+        if self.use_redis:
+            if trigger not in self.regex_data[str(group_id)]:
+                return 'Trigger not exist'
+            reply = ""
+            id_trigger_key = "miaowu.regex:{}:{}".format(group_id, trigger)
+            if self.database.srem(id_trigger_key, message) == 1:
+                reply = "Removed"
+            else:
+                reply = "Reply not exist"
+            if self.database.scard(id_trigger_key) == 0:
+                self.regex_data[str(group_id)].remove(trigger)
+                self.database.delete(id_trigger_key)
+                self.database.srem("miaowu.regex:{}".format(group_id), trigger)
+            return reply
+        else:
+            return 'Reply not exist'
