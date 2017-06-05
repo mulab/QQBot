ec5cd8bd68be33570900e7f2e03264d276fb6a26
Hubert Zhang <hubert.zyk@gmail.com>
2017-06-05 16:10:01 +0800

    add support for weixin

--- plugin.py
+++ plugin.py
@@ -2,7 +2,10 @@ class Plugin:
     command_description = "No description"
     priority = 0
 
-    def load_data(self, data_path="", redis_pool=None, webqq="127.0.0.1:5000"):
+    def weixin_enabled(self):
+        return False
+
+    def load_data(self, data_path="", redis_pool=None, webqq="127.0.0.1:5000", weixin=None):
         return
 
     def supported_commands(self):
